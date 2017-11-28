3ce14db4ef4721780a5f0b6ba83b3c200056edd2
Jardine <jardine.miller@gmail.com>
2017-11-28 13:45:56 +0000

    Admin/Restaurants now RESTful

--- controllers/admin_controller.rb
+++ controllers/admin_controller.rb
@@ -58,6 +58,28 @@ get '/admin/restaurants/new' do #RESTAURANT - NEW
   erb (:"/admin/restaurants/new")
 end
 
+post '/admin/restaurants' do #RESTAURANT - CREATE
+  @restaurant = Restaurant.new(params)
+  @restaurant.save
+  redirect to '/admin/restaurants'
+end
+
+get '/admin/restaurants/:id/edit' do #RESTAURANT - EDIT
+  @restaurant = Restaurant.find(params[:id])
+  erb (:"/admin/restaurants/edit")
+end
+
+put '/admin/restaurants/:id' do #RESTAURANT - UPDATE
+  Restaurant.new(params).update
+  redirect to '/admin/restaurants'
+end
+
+delete '/admin/restaurants/:id' do #RESTAURANT - DELETE
+  restaurant = Restaurant.find(params[:id])
+  restaurant.delete
+  redirect to '/admin/restaurants'
+end
+
 
 
 get '/admin/deals' do #DEALS - INDEX
